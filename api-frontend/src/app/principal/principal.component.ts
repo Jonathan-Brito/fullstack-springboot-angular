560548b7d5466e1ccc68d82fcf369a302071ad76
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2024-09-04 23:20:01 -0300

    Trabalhando com Listagem de clientes

--- api-frontend/src/app/principal/principal.component.ts
+++ api-frontend/src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
 export class PrincipalComponent {
   // Variável para visíbilidade dos botões
   btnCadastro:boolean = true;
+
+  // JSON de clientes
+  clientes:Cliente [] = [];
+
+  // Construtor
+  constructor(private servico:ClienteService){}
+
+  // Método de seleção
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retornoLista => this.clientes = retornoLista);
+  }
 
 }
