a600a763730a172c85d4fd9305ca21586bbbc5bc
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2024-09-06 23:09:12 -0300

    Rota de cadastro

--- api-backend/api/src/main/java/br/brito/api/controller/ClienteController.java
+++ api-backend/api/src/main/java/br/brito/api/controller/ClienteController.java
@@ -1,15 +1,23 @@
 package br.brito.api.controller;
 
+import br.brito.api.model.Cliente;
 import br.brito.api.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return clienteRepository.save(c); // c é o objeto contendo as caracteristicas
+    }
     @GetMapping("/")
     public String teste(){
         return "Hello World !";
