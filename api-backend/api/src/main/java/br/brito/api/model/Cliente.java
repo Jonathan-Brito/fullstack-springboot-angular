d230f0186820e3dcc461939ba1700b479851c2f0
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2024-09-06 22:50:49 -0300

    configurar nossa entidade, com ela podemos enviar e receber dados do tipo cliente em nossas rotas, além de gerar uma tabela em nosso banco MySQL

--- api-backend/api/src/main/java/br/brito/api/model/Cliente.java
+++ api-backend/api/src/main/java/br/brito/api/model/Cliente.java
@@ -1,4 +1,22 @@
 package br.brito.api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    private String nome;
+
+    private int idade;
+
+    private String cidade;
 }
