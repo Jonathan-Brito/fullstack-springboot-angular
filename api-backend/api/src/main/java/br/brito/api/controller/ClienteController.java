fb313fe65a5d3278ca768be2800155c82f367eab
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2024-09-06 23:02:40 -0300

    Annotation @Autowired

--- api-backend/api/src/main/java/br/brito/api/controller/ClienteController.java
+++ api-backend/api/src/main/java/br/brito/api/controller/ClienteController.java
@@ -1,11 +1,15 @@
 package br.brito.api.controller;
 
+import br.brito.api.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClienteController {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
     @GetMapping("/")
     public String teste(){
         return "Hello World !";
